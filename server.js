3f34a25d6550a3c81f8d501f78c16a4e1eefbb44
Christian Perfect <christianperfect@gmail.com>
2012-06-26 16:15:01 +0100

    tidy up, now I know what I'm doing

--- server.js
+++ server.js
@@ -1,51 +1,128 @@
 var http = require('http'),
 	faye = require('faye'),
-	fs = require('fs')
+	fs = require('fs'),
+	express = require('express')
 ;
 
 var wordList = fs.readFileSync('2of12inf.txt').toString().split('\n').map(function(word){return word.toUpperCase().replace(/[^A-Z]/g,'')});
-
-var bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
-bayeux.listen(1000);
-
 var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 function Player(data) {
 	this.name = data.name;
-	this.slug = this.name.replace(/\s/g,'-').replace(/[^a-zA-Z\-]/g,'');
+	this.slug = this.name.replace(/\s/g,'-').replace(/[^a-zA-Z\-_!~\(\)\$@]/g,'');
 }
 
 Player.prototype = {
 	name: 'Unnamed player',
 	score: 0,
 
+	state: function() {
+		return {
+			name: this.name,
+			slug: this.slug,
+			score: this.score
+		};
+	},
+
 	sendMessage: function(message,kind) {
 		kind = kind || 'chat';
-		game.client.publish('/message/'+this.name, {kind: kind, player: 'Nulac', message: message})
+		game.client.publish('/message/'+this.slug, {kind: kind, player: 'Nulac', message: message})
 	},
 
 	award: function(points) {
 		this.score += points;
-		game.client.publish('/player/score',{player: this.name, score: this.score});
+		game.client.publish('/player/score/'+this.slug,this.score);
 	}
 }
 
 function Game() {
-	this.players = [];
+	
+	//set up the server
+	var app = this.server = express.createServer();
+	app.configure(function () {
+		app.use(express.bodyParser());
+		app.use(express.static(__dirname + '/static'));
+	});
+	app.listen(8123);
 
+
+	//set up faye pub/sub
+	var bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
+	bayeux.attach(app);
+	this.client = bayeux.getClient();
+
+	//make subscriptions to faye channels
 	this.doSubscriptions();
+
+	//set up the addresses clients can access
+	this.doWebMethods();
+
+
+	// set up game
+
+	this.players = [];
 
 	this.init()
 }
 Game.prototype = {
 	usedLetters: '',
-	turn: null,
+	turn: 0,
+
+	doWebMethods: function() {
+		var app = this.server,
+			client = this.client,
+			game = this;
+		;
+
+		//to join a game
+		app.post('/join',function(req,res) {
+			var data = req.body;
+			var player = game.join(data);
+			var odata = game.state();
+			odata.you = player.state();
+			res.send(odata);
+		});
+
+		//to get the current state of the game
+		app.get('/state',function(req,res) {
+			res.send(game.state());
+		});
+
+
+		//404 handlers
+		function notFound(req,res) {
+			res.send('Not found',404);
+		}
+		app.get(/\/.*/,notFound);
+		app.post(/\/.*/,notFound);
+	},
+
+	doSubscriptions: function() {
+		var client = this.client;
+		var game = this;
+
+		client.subscribe('/play',function(data) {
+			var player = game.players[data.player];
+			word = data.word.toUpperCase().replace(/[^A-Z]/g,'');
+			game.tryWord(player,word);
+		});
+
+		//debug: echo all messages
+		client.subscribe('/**',function(data) {
+			console.log(data);
+		});
+	},
 
 	init: function() {
+		//set game state
 		this.usedLetters = '';
 		this.playedWords = [];
 		this.availableLetters = alphabet.split('');
-		this.client.publish('/turn/played/list',this.playedWords);
+
+		//tell the players
+		this.client.publish('/new-game','new game');
+
+		//if there are players, pick one to take a turn
 		if(this.players.length) {
 			this.turn = 0;
 			this.nextTurn();
@@ -53,66 +130,78 @@ Game.prototype = {
 	},
 
 	end: function() {
-		this.currentPlayer.score++;
-		this.client.publish('/players',this.players);
-		this.client.publish('/message',{kind: 'won', message: 'There are no playable letters, so '+this.currentPlayer.name+' wins!'});
+		//give the winning player 10 points
+		this.currentPlayer.award(10);
+
+		//tell the players
+		this.client.publish('/end-game',this.currentPlayer.name);
+
+		//start a new game
 		this.init();
 	},
 
-	doSubscriptions: function() {
-		var game = this;
-		var client = this.client = bayeux.getClient();
-		client.subscribe('/join',function(data) {
-			var player;
-			var name = data.name;
-			if(!(name in game.players)) {
-				player = game.addPlayer(data);
-				client.publish('/message', {kind: 'join', message: name+' joined'});
-			}
-			else {
-				player = game.players[name];
-				client.publish('/message', {kind: 'join', message: name+' rejoined'});
-			}
-			player.sendMessage('Hi, '+player.name+'!');
-			client.publish('/players',game.players);
+	join: function(data) {
+		var player;
 
-			if(game.players.length==1)
-				game.nextTurn();
-			
-			game.publishInit();
-		});
+		//decide if the player is already in the game
+		if(!(data.name in this.players)) {
+			//if not, add them
+			player = this.addPlayer(data)
+		}
+		else {
+			player = this.players[data.name];
+		}
 
-		client.subscribe('/turn',function(data) {
-			var player = game.players[data.player];
-			if(player!=game.currentPlayer && player) {
-				player.sendMessage("It isn't your turn.",'error');
-			}
-			else {
-				word = data.word.toUpperCase().replace(/[^A-Z]/g,'');
-				game.takeTurn(word);
-			}
-		});
-	},
-
-	addPlayer: function(data) {
-		var player = new Player(data);
-
-		this.players.push(player);
-		this.players[player.name] = player;
+		//say hi to the player
+		player.sendMessage('Hi, '+player.name+'!');
 		
 		return player;
 	},
-	
-	nextTurn: function(data) {
-		this.turn = (this.turn+1) % this.players.length;
-		this.currentPlayer = this.players[this.turn];
-		this.publishState();
+
+	addPlayer: function(data) {
+		//create the player
+		var player = new Player(data);
+
+		//add it to the list
+		this.players.push(player);
+		this.players[player.name] = player;
+
+		//tell the other players
+		this.client.publish('/new-player',player.state());
+
+		//if this is the first player, it's their turn
+		if(this.players.length==1)
+			this.nextTurn();
+
+		return player
 	},
 
-	takeTurn: function(word) {
+	removePlayer: function(player) {
+		//remove the player from the list
+		var i = this.players.indexOf(player);
+		this.players.splice(i,1);
+		delete this.players[player.name];
+
+		//tell the other players
+		this.client.publish('/player/left',player.name);
+		this.sendMessage({message: player.name+' left the game.', kind: 'player-left'});
+
+		//now it's the next player's turn
+		if(this.currentPlayer == player) {
+			this.turn--;
+			this.nextTurn();
+		}
+	},
+
+	tryWord: function(player,word) {
+		//check it's the player's turn
+		if(player!=this.currentPlayer) {
+			player.sendMessage("It isn't your turn.",'error');
+			return;
+		}
+
+		//check the word is valid
 		try {
-			if(wordList.indexOf(word)==-1)
-				throw(new Error("I don't recognise the word "+word));
 			if(wordList.indexOf(word)==-1)
 				throw(new Error("I don't recognise the word "+word));
 			if(word.length<4)
@@ -121,35 +210,66 @@ Game.prototype = {
 				throw(new Error('The first letter of your word must be in the previous word.'));
 			if(this.usedLetters.indexOf(word[0])>0)
 				throw(new Error(word[0]+' has been used.'));
-
-			this.usedLetters += word[0];
-			var usedLetters = this.usedLetters;
-			this.availableLetters = alphabet.split('').filter(function(l){ return usedLetters.indexOf(l)==-1 && word.indexOf(l)>0; });
-			this.playedWords.push(word);
-			this.client.publish('/turn/played',word);
-			this.client.publish('/turn/played/list',this.playedWords);
-			if(this.availableLetters.length) {
-				this.nextTurn();
-			} else {
-				this.end();
-			}
 		}
+		//if not, tell the player
 		catch(e) {
 			this.currentPlayer.sendMessage(e.message,'badword');
+			return;
 		}
+
+		//if the word is valid, play it
+		this.playWord(word);
 	},
 
-	publishState: function() {
+	playWord: function(word) {
+		var game = this;
+
+		//keep track of which letters have been used and are available
+		this.usedLetters += word[0];
+		this.availableLetters = alphabet.split('').filter(function(l){return game.usedLetters.indexOf(l)==-1 && word.indexOf(l)>0});
+		this.playedWords.push(word);
+
+		//tell the players
+		this.client.publish('/played',{player: this.currentPlayer.name, word: word});
+		this.client.publish('/available-letters',this.availableLetters);
+
+		//give the player a point for each letter
+		this.currentPlayer.award(word.length);
+
+		//if there are unused letters in the word, it's the next player's turn
+		if(this.availableLetters.length)
+			this.nextTurn();
+		//if not, that's the end of the game
+		else
+			this.end();
+	},
+
+	nextTurn: function(data) {
+		//work out whose turn it is now
+		this.turn = (this.turn+1) % this.players.length;
+		this.currentPlayer = this.players[this.turn];
+
+		//tell the players
 		this.client.publish('/current-player',this.currentPlayer.name);
-		this.client.publish('/availableLetters',this.availableLetters);
 	},
 
-	publishInit: function() {
-		this.client.publish('/init',{
-			availableLetters: this.availableLetters,
+	sendMessage: function(data) {
+		if(typeof data=='string')
+			data = {kind: 'chat',message: data};
+
+		data.kind = data.kind || 'chat';
+		data.player = 'Nulac';
+
+		this.client.publish('/message',data);
+	},
+
+	state: function() {
+		return {
+			players: this.players.map(function(p) {return p.state()}),
 			playedWords: this.playedWords,
+			availableLetters: this.availableLetters,
 			currentPlayer: this.currentPlayer.name
-		});
+		}
 	}
 }
 
---
+++ static/js/thing.js
@@ -0,0 +1,194 @@
+//Knockout rubbish
+ko.bindingHandlers.class = {
+	init: function(el,value) {
+		value = ko.utils.unwrapObservable(value());
+		ko.utils.domData.set(el,'class','');
+	},
+	update: function(el,value) {
+		value = ko.utils.unwrapObservable(value());
+		var oldValue = ko.utils.domData.get(el,'class');
+		ko.utils.domData.set(el,'class',value);
+		$(el).removeClass(oldValue).addClass(value);
+	}
+};
+
+function subscribeObservable(client,path) {
+	var obs = ko.observable();
+	client.subscribe(path,function(d) {
+		obs(d);
+	});
+	return obs;
+}
+
+function subscribeArray(client,path) {
+	var obs = ko.observableArray();
+	client.subscribe(path,function(d) {
+		obs(d);
+	});
+	return obs;
+}
+
+
+//The actual thing
+$(function() {
+
+	//set up faye pub/sub
+	var client = window.client = new Faye.Client('/faye');
+
+	function Message(data) {
+		this.player = data.player;
+		this.kind = data.kind || '';
+		this.message = data.message;
+	}
+
+	function Player(data) {
+		this.name = data.name;
+		this.slug = data.slug;
+		this.score = subscribeObservable(client,'/player/score/'+this.slug);
+		this.score(data.score);
+	}
+
+	function Nulac() {
+		var game = this;
+
+		//is the player currently in the game?
+		this.playing = ko.observable(false);
+		//the player's name
+		this.myName = ko.observable(null);
+
+		//list of all the players in the game
+		this.players = ko.observableArray([]);
+		this.playerNames = {};
+
+		//list of chat messages, game narration, etc.
+		this.messages = ko.observableArray([]);
+
+		//the name of the player whose turn it is
+		this.currentPlayer = subscribeObservable(client,'/current-player');
+
+		//the letters that can be used
+		this.availableLetters = subscribeObservable(client,'/available-letters');
+		this.playedWords = subscribeArray(client,'/turn/played/list');
+
+		//when a game starts
+		client.subscribe('/new-game',function() {
+			game.receiveMessage({kind: 'new-game', message: 'A new round has started.'});
+		});
+
+		//when a game ends
+		client.subscribe('/end-game',function(player) {
+			game.receiveMessage({kind: 'end-game', message: 'There are no available letters, so '+player+' wins this round.'});
+		});
+
+		//when a player joins
+		client.subscribe('/new-player',function(data) {
+			var player = game.addPlayer(data);
+			game.receiveMessage({kind: 'join', message: player.name+' has joined the game.'});
+		});
+
+		//when a chat message is sent
+		client.subscribe('/message',function(data) {
+			game.receiveMessage(data)
+		});
+
+		//when a player plays a word
+		client.subscribe('/played',function(data) {
+			//add the word to the list of played words
+			game.playedWords.push(data.word);
+
+			//tell the player
+			game.receiveMessage({player: data.player, kind: 'played', message: 'played '+data.word});
+		});
+
+		//debug: echo all channels
+		client.subscribe('/**',function(data) {
+			console.log(data);
+		});
+
+		//when the current player changes, tell the player
+		client.subscribe('/current-player',function(player) {
+			game.setCurrentPlayer(player);
+		});
+	}
+	Nulac.prototype = {
+		join: function(el) {
+			var game = this;
+			var name = $(el).find('.name').val();
+			this.myName(name);
+			$.post('/join',{name: name})
+				.success(function(data) {
+					game.updateState(data);
+					game.playing(true);
+				})
+				.error(function(data) {
+					console.log(data);
+				})
+			;
+		},
+		
+		addPlayer: function(data) {
+			var player = new Player(data);
+			this.playerNames[player.name] = player;
+			this.players.push(player);
+			return player;
+		},
+
+		updateState: function(data) {
+			console.log(data);
+			var game = this;
+
+			//create the list of players
+			this.players([]);
+			var playerNames = this.playerNames = {};
+			for(var i=0;i<data.players.length;i++) {
+				this.addPlayer(data.players[i]);
+			}
+
+			//record the player's name
+			this.myName(data.you.name);
+
+			//listen for private messages
+			client.subscribe('/message/'+data.you.slug,function(data) {
+				game.receiveMessage(data)
+			});
+
+			//record the played words and available letters
+			this.playedWords(data.playedWords);
+			this.availableLetters(data.availableLetters);
+
+			//record the current player
+			this.setCurrentPlayer(data.currentPlayer);
+		},
+
+		receiveMessage: function(data) {
+			console.log(data.kind,data.message);
+			this.messages.splice(0,0,new Message(data));
+		},
+
+		setCurrentPlayer: function(player) {
+			this.currentPlayer(player);
+			var message = player==this.myName() ? "It's your turn." : "It's "+player+"'s turn.";
+			this.receiveMessage({kind: 'currentplayer', message: message});
+		},
+
+		sendMessage: function(el) {
+			var message = $(el).find('.message').val();
+			$(el).find('.message').val('');
+
+			if(message.toUpperCase() == message) {
+				client.publish('/play',{player: this.myName(), word: message});
+			}
+			else {
+				client.publish('/message',{ 
+					player: this.myName(), 
+					kind: 'chat',
+					message: message 
+				});
+			}
+		}
+	};
+
+	var game = window.game = new Nulac();
+	ko.applyBindings(game);
+
+});
